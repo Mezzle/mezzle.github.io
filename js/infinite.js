ce0e759f91ebbc8108cbb7c10bc90073ce1db29f
Martin Meredith <martin@sourceguru.net>
2016-06-26 22:54:34 +0100

    Cleanup JS

--- js/infinite.js
+++ js/infinite.js
@@ -1,13 +1,13 @@
-$(function() {
+$(function () {
 
-    var postURLs,
-        isFetchingPosts = false,
-        shouldFetchPosts = true,
-        postsToLoad = $(".posts").children().length,
-        loadNewPostsThreshold = 3000;
+    var postURLs;
+    var isFetchingPosts = false;
+    var shouldFetchPosts = true;
+    var postsToLoad = $(".posts").children().length;
+    var loadNewPostsThreshold = 3000;
 
     // Load the JSON file containing all URLs
-    $.getJSON('/all_posts.json', function(data) {
+    $.getJSON('/all_posts.json', function (data) {
         postURLs = data["posts"];
 
         // If there aren't any more posts available to load than already visible, disable fetching
@@ -20,13 +20,14 @@ $(function() {
         shouldFetchPosts = false;
 
     // Are we close to the end of the page? If we are, load more posts
-    $(window).scroll(function(e){
+    $(window).scroll(function (e) {
+        console.log(isFetchingPosts, shouldFetchPosts);
         if (!shouldFetchPosts || isFetchingPosts) return;
 
-        var windowHeight = $(window).height(),
-            windowScrollPosition = $(window).scrollTop(),
-            bottomScrollPosition = windowHeight + windowScrollPosition,
-            documentHeight = $(document).height();
+        var windowHeight = $(window).height();
+        var windowScrollPosition = $(window).scrollTop();
+        var bottomScrollPosition = windowHeight + windowScrollPosition;
+        var documentHeight = $(document).height();
 
         // If we've scrolled past the loadNewPostsThreshold, fetch posts
         if ((documentHeight - loadNewPostsThreshold) < bottomScrollPosition) {
@@ -35,7 +36,7 @@ $(function() {
     });
 
     // Fetch a chunk of posts
-    function fetchPosts() {
+    var fetchPosts = function () {
         // Exit if postURLs haven't been loaded
         if (!postURLs) return;
 
@@ -43,37 +44,37 @@ $(function() {
 
         // Load as many posts as there were present on the page when it loaded
         // After successfully loading a post, load the next one
-        var loadedPosts = 0,
-            postCount = $(".posts").children().length,
-            callback = function() {
-                loadedPosts++;
-                var postIndex = postCount + loadedPosts;
+        var loadedPosts = 0;
+        var postCount = $(".posts").children().length;
+        var callback = function () {
+            loadedPosts++;
+            var postIndex = postCount + loadedPosts;
 
-                if (postIndex > postURLs.length-1) {
-                    disableFetching();
-                    return;
-                }
+            if (postIndex > postURLs.length - 1) {
+                disableFetching();
+                return;
+            }
 
-                if (loadedPosts < postsToLoad) {
-                    fetchPostWithIndex(postIndex, callback);
-                } else {
-                    isFetchingPosts = false;
-                }
-            };
+            if (loadedPosts < postsToLoad) {
+                fetchPostWithIndex(postIndex, callback);
+            } else {
+                isFetchingPosts = false;
+            }
+        };
 
         fetchPostWithIndex(postCount + loadedPosts, callback);
-    }
+    };
 
-    function fetchPostWithIndex(index, callback) {
+    var fetchPostWithIndex = function (index, callback) {
         var postURL = postURLs[index];
 
-        $.get(postURL, function(data) {
+        $.get(postURL, function (data) {
             $(data).find(".post").appendTo(".posts");
             callback();
         });
-    }
+    };
 
-    function disableFetching() {
+    var disableFetching = function () {
         shouldFetchPosts = false;
         isFetchingPosts = false;
         $(".infinite-spinner").fadeOut();
